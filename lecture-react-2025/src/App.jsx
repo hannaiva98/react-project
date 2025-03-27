2cab2042b7014a1a85650b86733182bdf92d96a0
FirstName LastName <email@address>
2025-03-27 12:03:21 +0200

    Create list of lessons

---
+++ lecture-react-2025/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const items = ["Mathematics", "Informatics", "PH", "Biology", "Chemistry"];
+
+function Lessons() {
+  return (
+    <div style={{ padding: "60px", fontFamily: "Arial" }}>
+      <h2>My School lessons</h2>
+      <ul>
+        {items.map((element, index) => (
+          <li key={index}>{element}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Lessons;
